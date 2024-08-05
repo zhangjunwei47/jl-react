2f543996252a73d1767f07d012669c129e13d8cb
codyzhangchao <codyzhangchao@didiglobal.com>
2024-08-05 15:54:23 +0800

    [other]

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
+import Button from './components/Button/button'
+import './styles/index.scss'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World !
-        </p>
-        
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+            <Button btnType="primary" size="lg" onClick={()=>alert('click....')}>Primary Button</Button>
+            <Button btnType="link" href="https://www.example.com">Link Button</Button>
+            <Button disabled>Disabled Button</Button>
     </div>
   );
 }
