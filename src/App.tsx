4bdaaa55afc9a94ddd69898884a4833ae9db8ae5
codyzhangchao <codyzhangchao@didiglobal.com>
2024-08-05 17:47:30 +0800

    [opti][增加icon图标][测试建议：无]

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,22 @@ import React from 'react';
 import logo from './logo.svg';
 import Button from './components/Button/button'
 import './styles/index.scss'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+import Icon from './components/Icon/inon';
 
 function App() {
   return (
     <div className="App">
-            <Button btnType="primary" size="lg" onClick={()=>alert('click....')}>Primary Button</Button>
-            <Button btnType="link" href="https://www.example.com">Link Button</Button>
-            <Button disabled>Disabled Button</Button>
+      <h1>Button</h1>
+      <div>
+        <Button btnType="primary" size="lg" onClick={() => alert('click....')}>Primary Button</Button>
+        <Button btnType="link" href="https://www.example.com">Link Button</Button>
+        <Button disabled>Disabled Button</Button>
+      </div>
+      <h1>Icon</h1>  
+      <div>
+        <Icon icon={faCoffee} theme="success" />
+      </div>
     </div>
   );
 }
